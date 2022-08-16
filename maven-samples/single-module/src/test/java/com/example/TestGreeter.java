9842cec78d10d5afbcdf743171005824ae05ddd9
abattistin-work <110538977+abattistin-work@users.noreply.github.com>
2022-08-16 10:17:04 +0100

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -22,7 +22,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
+    String someone = "World, Thanks";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
